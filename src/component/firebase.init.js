8801ae4be488334ef8835da81869ac25293afa34
Shakaet <abdshakaet@gmail.com>
2024-12-27 00:25:54 +0600

    add

--- src/component/firebase.init.js
+++ src/component/firebase.init.js
@@ -6,12 +6,18 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDuwW1pTJR6xFD3v3Ds_b72nDv1DDhYNLo",
-  authDomain: "hotel-booking-5ea8c.firebaseapp.com",
-  projectId: "hotel-booking-5ea8c",
-  storageBucket: "hotel-booking-5ea8c.firebasestorage.app",
-  messagingSenderId: "560628547104",
-  appId: "1:560628547104:web:41661412c0bc5920ee4645"
+  // apiKey: "AIzaSyDuwW1pTJR6xFD3v3Ds_b72nDv1DDhYNLo",
+  // authDomain: "hotel-booking-5ea8c.firebaseapp.com",
+  // projectId: "hotel-booking-5ea8c",
+  // storageBucket: "hotel-booking-5ea8c.firebasestorage.app",
+  // messagingSenderId: "560628547104",
+  // appId: "1:560628547104:web:41661412c0bc5920ee4645"
+  apiKey:import.meta.env.VITE_apiKey, 
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
